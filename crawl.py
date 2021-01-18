6103dbb2771e4c5687136dda5f3e6eaa431f5a04
Restyled.io <commits@restyled.io>
2021-01-18 14:37:52 +0000

    Restyled by black

--- crawl.py
+++ crawl.py
@@ -23,7 +23,7 @@ keyword = "코알라"
 # 키워드 설정하기 ( 각 단어를 띄워쓰기 !! )
 # 정확하게 일치하는 단어
 ## " "
-accurate = ["\"정확\"", "\"자주\""]
+accurate = ['"정확"', '"자주"']
 
 # 반드시 포함
 # %2B
@@ -33,22 +33,24 @@ mustin = ["%2B냠냠", "%2B포함"]
 # -
 exceptf = [" -광고", " -빼고"]
 
-searchfor = keyword+"+"+mustin[0]+exceptf[0]
+searchfor = keyword + "+" + mustin[0] + exceptf[0]
 
 driver.get(
-    "https://search.naver.com/search.naver?where=post&sm=tab_jum&query="+searchfor)
+    "https://search.naver.com/search.naver?where=post&sm=tab_jum&query=" + searchfor
+)
 
 
-'''
+"""
 媛� 寃뚯떆臾� 而⑦뀒�대꼫 : li.sh_blog_top
 寃뚯떆臾� �쒕ぉ �댁쓽 留곹겕 : li.sh_blog_top dt a �대��� href �띿꽦
 寃뚯떆臾� �띿뒪�� : div.se-module.se-module-text p span <- 臾몄옣�⑥쐞濡� �쒓렇媛� �щ젮�덉쓬
 �댁떆�쒓렇 : span.ell
 怨듦컧 �� : em.u_cnt._count
-'''
-with open("summary.csv",  "w", encoding='utf-8', newline='') as csv_fp:
+"""
+with open("summary.csv", "w", encoding="utf-8", newline="") as csv_fp:
     writer = csv.DictWriter(
-        csv_fp, fieldnames=["num", "content", "img", "sticker", "like", "tags"])
+        csv_fp, fieldnames=["num", "content", "img", "sticker", "like", "tags"]
+    )
     writer.writeheader()
 
 bloglinklist = driver.find_elements_by_css_selector("li.sh_blog_top dt a")
@@ -56,10 +58,10 @@ bloglinklist = driver.find_elements_by_css_selector("li.sh_blog_top dt a")
 k = 1
 
 for i in bloglinklist:
-    link = i.get_attribute('href')
+    link = i.get_attribute("href")
     path = link[-12:]
 
-    filename = str(k)+'.txt'
+    filename = str(k) + ".txt"
     k += 1
     # download_naver_blog �덉쓽 run �⑥닔 �ъ슜!
     download_naver_blog.run(link, filename)
@@ -68,14 +70,16 @@ for i in bloglinklist:
 
     driver.get(link)
     driver.switch_to_default_content  # 상위 프레임으로 전환
-    driver.switch_to.frame('mainFrame')  # cafe_main 프레임으로 전환
+    driver.switch_to.frame("mainFrame")  # cafe_main 프레임으로 전환
 
     html = driver.page_source  # 현재 페이지의 주소를 반환
-    soup = bs(html, 'html.parser')
+    soup = bs(html, "html.parser")
 
     # open csv file
 
-    with open("out/"+path + '/' + filename, 'r') as txtfile, open('summary.csv', 'a', encoding='utf-8') as csvfileout:
+    with open("out/" + path + "/" + filename, "r") as txtfile, open(
+        "summary.csv", "a", encoding="utf-8"
+    ) as csvfileout:
         parser = Parser(path, True)
         line = txtfile.read().replace("\n", " ")
         imgCnt = parser.imgCount()
@@ -93,22 +97,31 @@ for i in bloglinklist:
         # # postUrl = html.select_one("li.allview a").attrs["href"]
 
         # 태그
-        strtag = ''
-        tags = soup.find_all('span', class_='ell')
+        strtag = ""
+        tags = soup.find_all("span", class_="ell")
         for tag in tags:
             tag = tag.text
             strtag += tag
 
         # 공감
-        strheart = ''
-        heart = soup.find_all('em', class_='u_cnt _count')
+        strheart = ""
+        heart = soup.find_all("em", class_="u_cnt _count")
         for hrt in heart:
             hrt = hrt.text
             strheart += hrt
 
         # fp_full.close()
 
-        writer = csv.DictWriter(csvfileout, fieldnames=[
-                                "num", "content", "img", "sticker", "like", "tags"])
-        writer.writerow({'num': filename.rstrip('.txt'), 'content': line,
-                         'img': imgCnt, 'sticker': stiCnt, 'like': strheart, 'tags': strtag})
+        writer = csv.DictWriter(
+            csvfileout, fieldnames=["num", "content", "img", "sticker", "like", "tags"]
+        )
+        writer.writerow(
+            {
+                "num": filename.rstrip(".txt"),
+                "content": line,
+                "img": imgCnt,
+                "sticker": stiCnt,
+                "like": strheart,
+                "tags": strtag,
+            }
+        )
--- download_naver_blog.py
+++ download_naver_blog.py
@@ -11,30 +11,30 @@ import requests
 from bs4 import BeautifulSoup
 
 
-#import parsing_blog
+# import parsing_blog
 from parsing_blog import Parser
 import utils
 
-out_path = 'out'
-folder_path = ''
+out_path = "out"
+folder_path = ""
 
 
 def crawler(blog_url, path, file_name):
     parser = Parser(path, True)
     try:
-        soup = BeautifulSoup(requests.get(blog_url).text, 'lxml')
-        html = BeautifulSoup(requests.get(blog_url).text, 'html.parser')
+        soup = BeautifulSoup(requests.get(blog_url).text, "lxml")
+        html = BeautifulSoup(requests.get(blog_url).text, "html.parser")
 
-        with open(path + '/' + 'full_' + file_name,  "w", encoding='utf-8') as fp_full:
+        with open(path + "/" + "full_" + file_name, "w", encoding="utf-8") as fp_full:
             fp_full.write(str(soup))
 
-        with open(path + '/' + file_name,  "w", encoding='utf-8') as fp:
-            txt = ''
-            if 'se_component' in str(soup):
-                for sub_content in soup.select('div.se_component'):
+        with open(path + "/" + file_name, "w", encoding="utf-8") as fp:
+            txt = ""
+            if "se_component" in str(soup):
+                for sub_content in soup.select("div.se_component"):
                     txt += parser.parsing(sub_content)
             else:
-                for sub_content in soup.select('div.se-component'):
+                for sub_content in soup.select("div.se-component"):
                     txt += parser.parsing(sub_content)
             fp.write(txt)
 
@@ -68,48 +68,48 @@ def crawler(blog_url, path, file_name):
 def run(url, output):
     global out_path
     if not utils.check_out_folder():
-        print('폴더 생성에 실패했습니다.')
+        print("폴더 생성에 실패했습니다.")
         exit(-1)
 
-    save_folder_path = ''
-    redirect_url = ''
+    save_folder_path = ""
+    redirect_url = ""
     redirect_url = Parser.redirect_url(url)
-    #print('redirect url :' + redirect_url)
-    for item in redirect_url.split('&'):
-        if item.find('logNo=') >= 0:
-            #item[redirect_url.find('logNo='), len('logNo='):]
-            value = item.split('=')
-            #print('content name: ' + value[1])
-            save_folder_path = out_path + '/' + value[1]
+    # print('redirect url :' + redirect_url)
+    for item in redirect_url.split("&"):
+        if item.find("logNo=") >= 0:
+            # item[redirect_url.find('logNo='), len('logNo='):]
+            value = item.split("=")
+            # print('content name: ' + value[1])
+            save_folder_path = out_path + "/" + value[1]
             if utils.check_folder(save_folder_path):
-                #print(value[1] + ' 폴더를 생성했습니다. ')
-                if utils.check_folder(save_folder_path+'/img'):
-                    print(value[1] + '와 ' + value[1] + 'img 폴더를 생성했습니다. ')
+                # print(value[1] + ' 폴더를 생성했습니다. ')
+                if utils.check_folder(save_folder_path + "/img"):
+                    print(value[1] + "와 " + value[1] + "img 폴더를 생성했습니다. ")
 
     if crawler(redirect_url, save_folder_path, output):
-        #print('완료하였습니다. out 폴더를 확인하세요.')
+        # print('완료하였습니다. out 폴더를 확인하세요.')
         pass
     else:
-        print(save_folder_path + '실패하였습니다.')
+        print(save_folder_path + "실패하였습니다.")
 
 
-if __name__ == '__main__':
-    #debug = True
+if __name__ == "__main__":
+    # debug = True
     debug = False
     if debug is False:
         if len(sys.argv) != 3:
+            print("python .\download_naver_blog.py [url of naver blog] [output]")
             print(
-                'python .\download_naver_blog.py [url of naver blog] [output]')
-            print(
-                'ex> python .\download_naver_blog.py https://blog.naver.com/chandong83/221951781607 blog.html')
+                "ex> python .\download_naver_blog.py https://blog.naver.com/chandong83/221951781607 blog.html"
+            )
             exit(-1)
         # print(url)
         url = sys.argv[1]
         output = sys.argv[2]
     else:
-        print('디버그 모드')
-        url = 'https://blog.naver.com/chandong83/221810614177'
-        output = 'parse.html'
+        print("디버그 모드")
+        url = "https://blog.naver.com/chandong83/221810614177"
+        output = "parse.html"
         print(url)
 
     run(url, output)
